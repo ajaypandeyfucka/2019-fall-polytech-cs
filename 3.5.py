f079567ada10ecf7a6926b3add7b5ecd9df7faf1
ajaypandeyfucka <55110136+ajaypandeyfucka@users.noreply.github.com>
2019-11-18 22:16:56 +0300

    Update 3.5.py

--- 3.5.py
+++ 3.5.py
@@ -1,23 +1,41 @@
-c = 0
-a, b = [ int(i) for i in input("введите 2 числа ").split() ]
-if (a == 0) or (b == 0):
-    c = 0
-elif a > 0 and b > 0:
-	for i in range(b):
-		c = c + a
-elif a < 0 and b < 0:
-	a = - a
-	b = - b
-	for i in range(b):
-		c = c + a
-elif a > 0 and b < 0:
-	b = b - b - b
-	for i in range(b):
-		c = c + a
-	c = -c
-else:
-	a = a - a - a
-	for i in range(b):
-		c = c + a
-	c = -c
-print(a, "*", b, "=", c)
+#Напишите функцию, которая выполняет умножение двух числе с 
+#использованием только +, -, << (умножение на 2) и >> (деление на 2).
+def multiplication(a, b):
+#экспериментально установлено: для ускорения работы 
+#первое число должно быть меньше второго,
+#что и осуществляют следующие 4 строки кода
+	if a > b:  
+		z = a
+		a = b
+		b = z 
+	bb = b
+	num = 0
+	c = 1
+	while a > c:
+		c = c << 1
+		num += 1
+	c = c >> 1
+	num = num - 1
+	b = b << num   # b * 2^num
+	a -= c 
+	while a > 0:
+		b += bb
+		a -=1
+	return b 
+def calculator(a, b):
+	if a == 0 or b == 0:
+		return 0
+	elif a > 0 and b > 0:
+		return multiplication(a, b)
+	elif a < 0 and b < 0:
+		a = -a 
+		b = -b
+		return multiplication(a, b)
+	elif a < 0 or b < 0:
+		a = abs(a)
+		b = abs(b)
+		return -multiplication(a, b)
+print("результат произведеня =", calculator(100000000, 1000000))
+print("результат произведеня =", calculator(3554856, 6748454))
+print("результат произведеня =", calculator(-4894564, 23346574))
+print("результат произведеня =", calculator(-4894564, -54948654))
