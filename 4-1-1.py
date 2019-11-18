1993ceef24fc621da2332ad7f28961d669188679
ajaypandeyfucka <55110136+ajaypandeyfucka@users.noreply.github.com>
2019-11-18 21:49:44 +0300

    Update 4-1-1.py

--- 4-1-1.py
+++ 4-1-1.py
@@ -1,8 +1,21 @@
 def determinant(p):
-	o = p[::-1]
-	if o == p:
+	T = 1
+	lst = list(p)
+	for i in range(len(lst) // 2):	
+		if lst[0] == lst[-1]:
+			lst.pop(0)
+			lst.pop(-1)
+		else:
+			T = 0
+			break
+	if T == 1:
 		print(p, "- палиндром")
 	else:
-		print(p, "- не палиндромом")
-p = input("Введите число или слово ")
+		print(p, "- не является палиндромом")
+p = input("введите слово/число ")
+for j in range(len(list(p))):
+	assert list(p)[j] != "/"
+	assert list(p)[j] != "*"
+	assert list(p)[j] != "-"
+	assert list(p)[j] != "+"
 determinant(p)
